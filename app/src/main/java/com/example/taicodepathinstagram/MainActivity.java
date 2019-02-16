2ee22d6f92471dc1c36c8a7c77e1148777ecbf4f
taismith <tasmith8@aggies.ncat.edu>
2019-02-16 15:15:29 -0500

    required options

--- app/src/main/java/com/example/taicodepathinstagram/MainActivity.java
+++ app/src/main/java/com/example/taicodepathinstagram/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.taicodepathinstagram;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
-
+    private AppCompatCheckBox checkbox;
+    private Button btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
-
+        btnSignup = findViewById(R.id.btnSignup);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +44,87 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        checkbox = (AppCompatCheckBox) findViewById(R.id.checkbox);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (!isChecked) {
+                    // show password
+
+                    etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    // hide password
+                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create the ParseUser
+                ParseUser user = new ParseUser();
+                // Set core properties
+                user.setUsername("joestevens");
+                user.setPassword("secret123");
+                user.setEmail("email@example.com");
+                // Set custom properties
+                user.put("phone", "650-253-0000");
+                // Invoke signUpInBackground
+                user.signUpInBackground(new SignUpCallback() {
+                    public void done(ParseException e) {
+                        if (e == null) {
+                           // goMainActivity();
+                            // Hooray! Let them use the app now.
+                        } else {
+                            Log.e(TAG, "Issue with signup");
+                            e.printStackTrace();
+                            return;
+                            // Sign up didn't succeed. Look at the ParseException
+                            // to figure out what went wrong
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        ParseUser.logInInBackground("tai", "test", new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                   // goMainActivity();
+                    // Hooray! The user is logged in.
+                } else {
+                    Log.e(TAG, "Issue with logInBackground");
+                    e.printStackTrace();
+                    return;
+                    // Signup failed. Look at the ParseException to see what happened.
+                }
+            }
+        });
+
+        //  ParseUser currentUser = ParseUser.getCurrentUser();
+        //if (currentUser != null) {
+//
+        //          // do stuff with the user
+        //   } else {
+
+        //goLoginScreen();
+        // show the signup or login screen
+        //}
+
     }
+
+
+    private void goLoginScreen() {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 
 
     private void login(String username, String password){
@@ -59,9 +145,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
     private void goMainActivity() {
-        Intent i = new Intent(this,InstagramActivity.class);
-        startActivity(i);
-        finish();
+      Intent i = new Intent(this,InstagramActivity.class);
+       startActivity(i);
+       finish();
     }
 
 }
